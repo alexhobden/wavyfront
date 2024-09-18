f9bbaaf1b85faa7929577cf6360612a742f31d95
alexhobden <62051018+alexhobden@users.noreply.github.com>
2024-09-18 22:34:42 +0200

    add title and icon

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -5,7 +5,7 @@ import WindowFrame from "./windowFrame"
 const Welcome: React.FC = () => {
     return (
         <WindowFrame>
-            <div className="w-[480px] h-[320px] flex justify-center items-center bg-white p-12 text-center font-wavy">
+            <div className="w-[480px] h-[320px] flex justify-center items-center text-[#04e200] bg-black p-12 text-center font-wavy">
                 <Typewriter
                     text="Hello and Welcome to Wavy. This is a Website inspired by the classic win98 Desktop where i plan to experiment and dump products of my creative processes. The Website is currently in Development so a lot of Things won't work yet, but take a look around and enjoy :)"
                     speed={40}
